fd361d1ce8138d22407d42d00f44181542fb4145
Kevin M. Turney <super996@optonline.net>
2016-11-18 18:59:48 -0500

    CreateCards

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,9 +2,24 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-
+/*
 if(cardOne === cardTwo || cardThree === cardFour){
 	alert("You found a match!");
 }else if(cardOne !== cardTwo || cardThree !== cardFour){
 	alert("Sorry, try again.");
 } 
+*/
+
+let table = document.getElementById('game-board');
+let cards = [];
+cards.length = 4;
+
+function createCards(){
+	for(let i = 0; i < cards.length; i++){
+		let newCards = document.createElement('div');
+		newCards.className = 'card';
+		table.appendChild("newCards");
+	}
+}
+
+createCards();
